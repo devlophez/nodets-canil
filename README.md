4109fdc90b5da9b80264878c3ab419e56d7bff25
lophez <devlophez@gmail.com>
2023-11-30 15:42:59 -0300

    Initial Settings

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-canil
+
+Node + Typescript
+
+### Pré-requisitos globais:
+npm i -g nodemon typescript ts-node
+
+### INSTALAÇÃO
+npm install
+
+## PARA RODAR O PROJETO:
+npm run start-dev
---
+++ src/server.ts
@@ -0,0 +1,42 @@
+// EXPRESS SERVER
+import express from 'express'
+
+// Configurar Variáveis de Ambiente
+import dotenv from 'dotenv'
+
+// Template Engine
+import mustache from 'mustache-express'
+
+// Configurar Pasta Estática/Pública
+import path from 'path'
+
+// Import da index.ts, em routes
+import mainRoutes from './routes/index'
+
+// não esquecer de criar o arquivo .env
+dotenv.config() 
+
+const server = express()
+
+/** Configuração do Template Engine */
+server.set('view engine', 'mustache')
+// não esquecer de criar a pasta 'views' dentro da 'src'
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+
+/**  Configuração de  Pasta Pública 
+ * 
+ *      CRIAR pasta 'public' na raiz do projeto
+*/
+server.use(express.static(path.join(__dirname, '../public')))
+
+/**     Configuração de Rotas */
+server.use(mainRoutes)
+
+
+server.use((req, res) =>{
+    res.send('Página não encontrada!')
+})
+
+/**     Configuração de porta do servidor */
+server.listen(process.env.PORT)
